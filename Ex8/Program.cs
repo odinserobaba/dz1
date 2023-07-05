71bb585016885145d12b1205e3a1c27adcae689c
odinserobaba <odinserobaba@gmail.com>
2023-07-05 14:56:36 +0300

    Задача 8

---
+++ Ex8/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача №8
+// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все четные числа до N
+Console.WriteLine("Введите число");
+string input = Console.ReadLine();
+int N = int.Parse(input);
+Console.WriteLine("Введенное число: {0}", N);
+for (int i = 1; i < N; i++)
+    if(i%2==0)
+        Console.WriteLine("Четное число: {0}",i);
