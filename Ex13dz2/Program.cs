e7d9eab3a3eee69af1d7ff0930bd121e08d4af8f
odinserobaba <odinserobaba@gmail.com>
2023-07-10 09:04:52 +0300

    Задача 13

---
+++ Ex13dz2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+int GetThirdDigit(int number)
+{
+
+    int secondDigit = number % 10;
+    return secondDigit;
+}
+if (number < 100)
+    Console.WriteLine("Третьей цифры нет");
+else
+    Console.WriteLine(GetThirdDigit(number));
