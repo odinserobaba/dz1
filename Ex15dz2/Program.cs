148e11b98962b235147925f377be45ac3772870e
odinserobaba <odinserobaba@gmail.com>
2023-07-10 09:12:46 +0300

    Задача 15

---
+++ Ex15dz2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+void GetDayOff(int number)
+{
+    if(number==6 || number==7)
+        Console.WriteLine("Да");
+    else
+        Console.WriteLine("Нет");
+}
+GetDayOff(number);
