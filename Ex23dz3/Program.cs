6491ec005bc9f16e74ca67ff84b3a401c0cea90d
odinserobaba <odinserobaba@gmail.com>
2023-07-12 09:29:50 +0300

    Задача 23

---
+++ Ex23dz3/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+*/
+Console.WriteLine("Введите N");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Таблица кубов чисел от 1 до N.");
+for(int i = 1; i<=n;i++)
+    Console.Write(" {0}",Math.Pow(i,3));
