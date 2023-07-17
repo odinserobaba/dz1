1bf877b8dae1e03a2af5d42065828e0ac9ec228a
odinserobaba <odinserobaba@gmail.com>
2023-07-17 09:44:02 +0300

    Задача 29

--- Ex29dz4/Program.cs
+++ Ex29dz4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+int li=0;
+int[] mass = new int[8];
+Console.WriteLine("Введите массив из 8 целочисленных элементов!");
+while(li<8)
+{
+    try
+    {
+        mass[li]= int.Parse(Console.ReadLine());
+        li++;
+    }
+    catch
+    {
+        Console.WriteLine("Ошибка, это не число!");
+        continue;
+    }
+}
+for(int i=0;i<8;i++)
+    Console.Write(mass[i]+" ");
